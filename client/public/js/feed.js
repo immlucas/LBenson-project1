b05c3251f9025c88418029e5d8bc0d28110ede8e
Lucas Benson <lucasbenson41@gmail.com>
2024-07-10 11:48:01 -0400

    Parts of Java code

---
+++ client/public/js/feed.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    function goToMaristEdu() {
+        goToLocation('http://my.marist.edu');
+    }
+
+    document.getElementById('portal_button').addEventListener('click', goToMaristEdu);
+
+    document.getElementById('portal_button_text').addEventListener('click', goToMaristEdu);
+});
--- client/public/js/global.js
+++ client/public/js/global.js
@@ -1,5 +1,3 @@
-
-// These get run when any page moves to another.
-function goToLocation(location) {
-	window.location = location; //change to page passed.
+function goToLocation(url) {
+    window.location.href = url;
 }
