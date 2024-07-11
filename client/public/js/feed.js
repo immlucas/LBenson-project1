e233d390af42afc17ffc5b35aa271c634a58df31
Lucas Benson <lucasbenson41@gmail.com>
2024-07-11 15:51:08 -0400

    Project 1

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -8,6 +8,64 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('portal_button_text').addEventListener('click', goToMaristEdu);
 });
 
+//for mouse trail 
+var dots = [],
+    mouse = {
+      x: 0,
+      y: 0
+    };
+
+var Dot = function() {
+  this.x = 0;
+  this.y = 0;
+  this.node = (function(){
+    var n = document.createElement("div");
+    n.className = "MouseTrail";
+    document.body.appendChild(n);
+    return n;
+  }());
+};
+
+Dot.prototype.draw = function() {
+  this.node.style.left = this.x + "px";
+  this.node.style.top = this.y + "px";
+};
+
+for (var i = 0; i < 12; i++) {
+  var d = new Dot();
+  dots.push(d);
+}
+
+
+function draw() {
+
+  var x = mouse.x,
+      y = mouse.y;
+      
+  dots.forEach(function(dot, index, dots) {
+    var nextDot = dots[index + 1] || dots[0];
+    
+    dot.x = x;
+    dot.y = y;
+    dot.draw();
+    x += (nextDot.x - dot.x) * .6;
+    y += (nextDot.y - dot.y) * .6;
+
+  });
+}
+
+addEventListener("mousemove", function(event) {
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+});
+
+function animate() {
+  draw();
+  requestAnimationFrame(animate);
+}
+
+animate();
+
 function feedItem(title, body, linkUrl, imageUrl) {
     this.Title = title;
     this.Body = body;
@@ -17,9 +75,9 @@ function feedItem(title, body, linkUrl, imageUrl) {
 
 let currentStories = [];
 
-currentStories.push(new feedItem("Story 1", "Body 1", "http://linkurl1.com", "images/marist3.jpg"));
-currentStories.push(new feedItem("Story 2", "Body 2", "http://linkurl2.com", "images/news_picnew.jpg"));
-currentStories.push(new feedItem("Story 3", "Body 3", "http://linkurl3.com", "images/hancock.jpeg"));
+currentStories.push(new feedItem("Welcome students", "Welcome to our college!", "http://linkurl1.com", "images/marist3.jpg"));
+currentStories.push(new feedItem("New food!", "Our cafe has new food!", "http://linkurl2.com", "images/maristcafe2.jpg"));
+currentStories.push(new feedItem("Our Hancock center", "Look how beautiful!", "http://linkurl3.com", "images/hancockcenterfinal.jpg"));
 
 displayItem();
 
